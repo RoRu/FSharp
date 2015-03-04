bddca7e510df15f3725d311fd23015d424f002a1
Roru <vovayumatov@gmail.com>
2015-03-04 23:09:29 +0300

    Started work on solution for functions of list

---
+++ list_func/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let ins l x = 
+  x::l
+
+[<EntryPoint>]
+let main argv = 
+  let ls = [1;2;3;4]
+  let n = []
+  let n = List.fold ins n ls
+  printfn "%A" n
+  0 
+
