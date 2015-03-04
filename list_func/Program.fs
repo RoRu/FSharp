10bf64cbf8fa90932a7e6de345fced637eff6407
Roru <vovayumatov@gmail.com>
2015-03-05 00:32:56 +0300

    Added Reverse, Filter and Map

--- list_func/Program.fs
+++ list_func/Program.fs
@@ -1,14 +1,41 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿// Solution for functions of List through List.fold
+// by Yumatov Vladimir
+// SPBSU 171 gr.
 
-let ins l x = 
-  x::l
+let Reverse ls = 
+  let ins l x = 
+    x :: l
+  let n = []
+  List.fold ins n ls
+
+let Filter f ls =
+  let foo n x = 
+    match f(x) with 
+    | true -> x::n
+    | false -> n
+  let l = []
+  Reverse (List.fold foo l ls)
+  
+let Map f ls = 
+  let foo n x =
+    f(x)::n
+  let l = []
+  Reverse (List.fold foo l ls)
 
 [<EntryPoint>]
 let main argv = 
-  let ls = [1;2;3;4]
-  let n = []
-  let n = List.fold ins n ls
-  printfn "%A" n
-  0 
+  let ls = [1; 2; 3; 4; 5; 6]
 
+  let n = Reverse ls
+  printfn "Reversed list %A is %A" ls n
+
+  let fil z = 
+    z % 2 = 0
+  let n = Filter fil ls
+  printfn "Even elements of %A are %A" ls n
+
+  let Dbl z = 
+    z * 2
+  let n = Map Dbl ls
+  printfn "Doubled elements of %A are %A" ls n
+  0 
