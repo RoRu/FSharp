963cfd25f97b02712fb90811499c58f8876c36a6
Roru <vovayumatov@gmail.com>
2015-03-05 22:36:45 +0300

    Added Horner; remade Map, Filter, Reverse; Added comments

--- list_func/Program.fs
+++ list_func/Program.fs
@@ -1,41 +1,54 @@
-﻿// Solution for functions of List through List.fold
+﻿// Solution for functions of List via List.fold
 // by Yumatov Vladimir
 // SPBSU 171 gr.
 
-let Reverse ls = 
-  let ins l x = 
-    x :: l
-  let n = []
-  List.fold ins n ls
+// Ожидаемое время: 2 часа
+// Реальное время: 3.5 часов
 
+// Task 9
+// List.iter : ('a -> unit) -> 'a list -> unit
+
+// Task 10 
+let Reverse ls =
+  List.fold (fun res elm -> elm::res) [] ls
+
+
+// Task 11 
 let Filter f ls =
-  let foo n x = 
-    match f(x) with 
-    | true -> x::n
-    | false -> n
-  let l = []
-  Reverse (List.fold foo l ls)
-  
+  List.foldBack (fun elm res -> 
+                 if f(elm) then elm::res else res) ls []
+
+
+// Task 12 
 let Map f ls = 
-  let foo n x =
-    f(x)::n
-  let l = []
-  Reverse (List.fold foo l ls)
+  List.foldBack (fun elm res -> f(elm)::res) ls []
+
+
+// Task 13 Horner's method
+let Horner a cfs = 
+  List.fold (fun res elm -> elm + res * a) 0 cfs
+
 
 [<EntryPoint>]
 let main argv = 
   let ls = [1; 2; 3; 4; 5; 6]
 
   let n = Reverse ls
-  printfn "Reversed list %A is %A" ls n
+  printfn "Reversed list %A is %A\n" ls n
 
   let fil z = 
     z % 2 = 0
   let n = Filter fil ls
-  printfn "Even elements of %A are %A" ls n
+  printfn "Filter: Even elements of %A are %A\n" ls n
 
   let Dbl z = 
     z * 2
   let n = Map Dbl ls
-  printfn "Doubled elements of %A are %A" ls n
+  printfn "Map: Doubled elements of %A are %A\n" ls n
+
+
+  let cfs = [4; 5; 9; 1] // 4x^3 + 5x^2 + 9x + 1
+  let x0 = 5
+  let n = Horner x0 cfs
+  printfn "Horner result with coefficients %A is %d\n" cfs n
   0 
